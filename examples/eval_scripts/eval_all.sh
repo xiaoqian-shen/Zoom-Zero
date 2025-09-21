5e56add37ec9e97d0b1e04a116d04ae0c4596b84
Xiaoqian Shen <64844805+xiaoqian-shen@users.noreply.github.com>
2025-09-21 22:16:04 +0800

    Update eval_all.sh

--- examples/eval_scripts/eval_all.sh
+++ examples/eval_scripts/eval_all.sh
@@ -5,26 +5,26 @@ OUTPUT_DIR=work_dirs_eval/$base_name
 prefix="base"
 
 if [[ "$NAME" == *"VideoMME"* ]]; then
-    bash examples/eval_scripts/eval_videomme.sh $full_model_path ${prefix} /home/xiaoqians/personal/dataset/Video-MME
-    bash examples/eval_scripts/eval_videomme.sh $full_model_path ${prefix}_sub /home/xiaoqians/personal/dataset/Video-MME
+    bash examples/eval_scripts/eval_videomme.sh $full_model_path ${prefix} ./dataset/Video-MME
+    bash examples/eval_scripts/eval_videomme.sh $full_model_path ${prefix}_sub ./dataset/Video-MME
 fi
 
 if [[ "$NAME" == *"MLVU"* ]]; then
-    bash examples/eval_scripts/eval_mlvu.sh $full_model_path $prefix /home/xiaoqians/personal/dataset/MLVU/MLVU
+    bash examples/eval_scripts/eval_mlvu.sh $full_model_path $prefix ./dataset/MLVU/MLVU
 fi
 
 if [[ "$NAME" == *"LVBench"* ]]; then
-    bash examples/eval_scripts/eval_lvb.sh $full_model_path $prefix /home/xiaoqians/personal/dataset/LVBench
+    bash examples/eval_scripts/eval_lvb.sh $full_model_path $prefix ./dataset/LVBench
 fi
 
 if [[ "$NAME" == *"NextGQA"* ]]; then
-    bash examples/eval_scripts/eval_nextgqa.sh $full_model_path $prefix /home/xiaoqians/personal/dataset/NExT-GQA
+    bash examples/eval_scripts/eval_nextgqa.sh $full_model_path $prefix ./dataset/NExT-GQA
 fi
 
 if [[ "$NAME" == *"ReXTime"* ]]; then
-    bash examples/eval_scripts/eval_rextime.sh $full_model_path $prefix /home/xiaoqians/personal/dataset/ReXTime
+    bash examples/eval_scripts/eval_rextime.sh $full_model_path $prefix ./dataset/ReXTime
 fi
 
 if [[ "$NAME" == *"CGBench"* ]]; then
-    bash examples/eval_scripts/eval_cgbench.sh $full_model_path $prefix /home/xiaoqians/personal/dataset/CG-Bench
+    bash examples/eval_scripts/eval_cgbench.sh $full_model_path $prefix ./dataset/CG-Bench
 fi
